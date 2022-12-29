b5191f5300c8b9f51d4f94c3a9f95a1c794fdb32
Anna Kövecsi <anna.kovecsi@deptagency.com>
2022-12-29 14:03:12 +0100

    Updating with uncommitted files

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { UniformContext } from "@uniformdev/context-react";
+import { createUniformContext } from "../lib/context/uniformContext";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const clientContext = createUniformContext();
+
+export default function MyApp({ Component, pageProps, serverUniformContext }) {
+  return (
+    <UniformContext context={serverUniformContext ?? clientContext}>
+      <Component {...pageProps} />
+    </UniformContext>
+  );
 }
